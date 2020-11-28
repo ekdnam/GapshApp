e7680123373225ca7d7b98e6328221807f16a2e1
ekdnam <adeetya.m@gmail.com>
2020-11-28 23:51:21 +0530

    Error in MainActivity.java. IllegalStateException

--- app/src/main/java/com/ekdnam/whatsappclone/MainActivity.java
+++ app/src/main/java/com/ekdnam/whatsappclone/MainActivity.java
@@ -3,6 +3,7 @@ package com.ekdnam.whatsappclone;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseException;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
 
@@ -21,25 +23,43 @@ import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * With EditText user can enter stuff
+     */
     private EditText mPhoneNumber, mCode;
     private Button mSend;
 
+    /**
+     * mCallbacks added in case authentication fails or something
+     */
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+
+    private String mVerificationID;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         FirebaseApp.initializeApp(this);
 
+        userIsLoggedIn();
+
         mPhoneNumber = findViewById(R.id.phoneNumber);
         mCode = findViewById(R.id.code);
 
         mSend = findViewById(R.id.send);
 
+        /**
+         * if mSend button is clicked
+         */
         mSend.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startPhoneNumberVerification();
+                if(mVerificationID != null){
+                    verifyPhoneNumberWithCode();
+                }
+                else {
+                    startPhoneNumberVerification();
+                }
             }
         });
 
@@ -53,9 +73,26 @@ public class MainActivity extends AppCompatActivity {
             public void onVerificationFailed(@NonNull FirebaseException e) {
 
             }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationID, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(verificationID, forceResendingToken);
+
+                mVerificationID = verificationID;
+                mSend.setText("Verify Code");
+            }
         };
     }
 
+    private void verifyPhoneNumberWithCode(){
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationID, mCode.getText().toString());
+        signInWithPhoneAuthCredential(credential);
+    }
+    /**
+     * Start user login process
+     *
+     * @param phoneAuthCredential
+     */
     private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
         FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
             @Override
@@ -68,14 +105,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void userIsLoggedIn() {
-        
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if(user != null){
+            startActivity(new Intent(getApplicationContext(), MainPageActivity.class));
+            finish();
+            return;
+        }
     }
 
+    /**
+     * Uses Firebase method to verify phone number
+     *
+     */
     private void startPhoneNumberVerification() {
         PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                mPhoneNumber.getText().toString(),
-                60,
-                TimeUnit.SECONDS,
+                mPhoneNumber.getText().toString(), // get number
+                60, // timeout = 60 units
+                TimeUnit.SECONDS, // units = seconds
                 this,
                 mCallbacks
         );

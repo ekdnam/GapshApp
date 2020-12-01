36aaf8ae2ad4a35ea7485370155c1057c908f027
ekdnam <adeetya.m@gmail.com>
2020-12-01 13:15:17 +0530

    add user name visibility

--- app/src/main/java/com/ekdnam/whatsappclone/UserObject.java
+++ app/src/main/java/com/ekdnam/whatsappclone/UserObject.java
@@ -15,4 +15,12 @@ public class UserObject {
     public String getName() {
         return name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }

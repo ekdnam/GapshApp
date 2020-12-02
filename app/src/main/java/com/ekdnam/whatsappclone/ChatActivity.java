7fc728fa57140961a577c73c551170c5c07700ba
ekdnam <adeetya.m@gmail.com>
2020-12-02 22:11:07 +0530

    pre display messages

--- app/src/main/java/com/ekdnam/whatsappclone/ChatActivity.java
+++ app/src/main/java/com/ekdnam/whatsappclone/ChatActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,6 +16,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.ekdnam.whatsappclone.Chat.MessageAdapter;
 import com.ekdnam.whatsappclone.Chat.MessageObject;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -31,12 +36,20 @@ public class ChatActivity extends AppCompatActivity {
 
     String chatID;
 
+    DatabaseReference mChatDb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
         chatID = getIntent().getExtras().getString("chatID");
+
+        mChatDb = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child("chat")
+                .child(chatID);
 
         Button mSend = findViewById(R.id.send);
         mSend.setOnClickListener(new View.OnClickListener() {
@@ -47,13 +60,55 @@ public class ChatActivity extends AppCompatActivity {
         });
 
         initializeRecyclerView();
+        getChatMessages();
+    }
+
+    private void getChatMessages() {
+        mChatDb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                if(dataSnapshot.exists()){
+                    String text = "", creatorID = "";
+                    if(dataSnapshot.child("text").getValue() != null){
+                        text = dataSnapshot.child("text").getValue().toString();
+                    }
+                    if(dataSnapshot.child("creator").getValue() != null){
+                        creatorID = dataSnapshot.child("creator").getValue().toString();
+                    }
+                    MessageObject mMessage = new MessageObject(dataSnapshot.getKey(), creatorID, text);
+                    messageList.add(mMessage);
+                    mChatLayoutManager.scrollToPosition(messageList.size() - 1);
+                    mChatAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void sendMessage(){
         EditText mMessage = findViewById(R.id.message);
 
         if(!mMessage.getText().toString().isEmpty()){
-            DatabaseReference newMessageDb = FirebaseDatabase.getInstance().getReference().child("chat").child(chatID).push();
+            DatabaseReference newMessageDb = mChatDb.push();
 
             Map newMessageMap = new HashMap<>();
             newMessageMap.put("text", mMessage.getText().toString());
@@ -65,7 +120,7 @@ public class ChatActivity extends AppCompatActivity {
     }
 
 
-
+    @SuppressLint("WrongConstant")
     private void initializeRecyclerView() {
         messageList = new ArrayList<>();
         mChat= findViewById(R.id.messageList);
